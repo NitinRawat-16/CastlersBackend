07292e2a6f135b725e33f4f8ab50a51e8a90c3bc
Nitin Rawat <nitin.rawat@mindruby.com>
2023-12-25 16:25:18 +0530

    Db Changes for voting module

---
+++ Database/Updates/ElectionDetails.sql
@@ -0,0 +1,32 @@
+﻿USE [Castlers]
+GO
+
+/****** Object:  Table [dbo].[ElectionDetails]    Script Date: 25-12-2023 16:21:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ElectionDetails](
+	[electionId] [int] IDENTITY(1,1) NOT NULL,
+	[tenderId] [int] NULL,
+	[startDate] [datetime] NOT NULL,
+	[endDate] [datetime] NOT NULL,
+	[status] [nvarchar](50) NULL,
+	[totalVoters] [int] NULL,
+	[totalVoted] [int] NULL,
+	[creationDate] [datetime] NULL,
+	[updationDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[electionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ElectionDetails]  WITH CHECK ADD FOREIGN KEY([tenderId])
+REFERENCES [dbo].[TenderDetails] ([tenderId])
+GO
+
+
---
+++ Database/Updates/Sp_AddElectionDetails.sql
@@ -0,0 +1,44 @@
+﻿USE [Castlers]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_AddElectionDetails]    Script Date: 25-12-2023 16:22:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[usp_AddElectionDetails](
+  -- Parameters
+  @TenderId INT,
+  @StartDate DATETIME,
+  @EndDate DATETIME,
+  @Status NVARCHAR(50),
+  @TotalVoters INT,
+  @TotalVoted INT,
+  @CreationDate DATETIME,
+  @UpdationDate DATETIME,
+  @ElectionId INT OUTPUT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		
+		INSERT INTO ElectionDetails
+		VALUES (@TenderId, @StartDate, @EndDate, @Status, @TotalVoters, @TotalVoted,
+		@CreationDate, @UpdationDate)
+
+
+		SET @ElectionId = (SELECT SCOPE_IDENTITY())
+
+	END TRY
+	BEGIN CATCH
+	END CATCH
+END;
+GO
+
+
